3c0ddf3d9195bf1a85a2efa8f938fddc89c41cc7
Viv Lim <git@vvn.space>
2025-05-24 17:43:31 -0700

    update logged case to only write its log when there is a failure or error

--- sink/unittest.py
+++ sink/unittest.py
@@ -1,62 +1,36 @@
 import unittest, logging, sys
-# https://stackoverflow.com/a/15969985
 
-class LogThisTestCase(type):
-    def __new__(cls, name, bases, dct):
-        # if the TestCase already provides setUp, wrap it
-        if 'setUp' in dct:
-            setUp = dct['setUp']
-        else:
-            setUp = lambda self: None
+class _BufferingHandler(logging.Handler):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.records = []
 
-        def wrappedSetUp(self):
-            # for hdlr in self.logger.handlers:
-            #    self.logger.removeHandler(hdlr)
-            self.hdlr = logging.StreamHandler(sys.stdout)
-            self.logger.addHandler(self.hdlr)
-            setUp(self)
-        dct['setUp'] = wrappedSetUp
+    def emit(self, record):
+        self.records.append(record)
 
-        # same for tearDown
-        if 'tearDown' in dct:
-            tearDown = dct['tearDown']
-        else:
-            tearDown = lambda self: None
-
-        def wrappedTearDown(self):
-            tearDown(self)
-            self.logger.removeHandler(self.hdlr)
-
-        def wrappedRun(self, result=None):
-            print("wrapped run")
-            self.logger.error("runnn")
-            self.run(result)
-
-        dct['tearDown'] = wrappedTearDown
-        dct['run'] = wrappedRun
-
-        # return the class instance with the replaced setUp/tearDown
-        return type.__new__(cls, name, bases, dct)
 
 class LoggedTestCase(unittest.TestCase):
+    """Test case which accumulates logs as it runs and only prints them if there is a failure or error"""
     def __init__(self, *args):
         super().__init__(*args)
 
-        self.logger = logging.getLogger("unittestLogger")
-        self.logger.setLevel(logging.DEBUG) # or whatever you prefer
+        self.logger = logging.getLogger("bufferingLogger")
+        self.logger.setLevel(logging.DEBUG)
 
     def setUp(self):
         super().setUp()
-        # for hdlr in self.logger.handlers:
-        #    self.logger.removeHandler(hdlr)
-        self.hdlr = logging.StreamHandler(sys.stdout)
-        self.logger.addHandler(self.hdlr)
+        for hdlr in self.logger.handlers:
+           self.logger.removeHandler(hdlr)
+        self.logbuf = _BufferingHandler()
+        self.logger.addHandler(self.logbuf)
 
     def tearDown(self):
         super().tearDown()
-        self.logger.removeHandler(self.hdlr)
+        self.logger.removeHandler(self.logbuf)
 
-    def wrappedRun(self, result=None):
-        print("wrapped run")
-        self.logger.error("runnn")
-        self.run(result)
+    def run(self, result=None):
+        super().run(result)
+        if result and (len(result.failures) + len(result.errors)) > 0:
+            sh = logging.StreamHandler(sys.stdout)
+            for r in self.logbuf.records:
+                sh.emit(r)
